400a27c4486b087ef077e416cd6f2210da8fb57f
Chinh <cqnguyen21st@yahoo.com>
2016-03-19 01:06:46 -0700

    Test

---
+++ app/src/main/java/com/example/cqngu/doggie/Dog.java
@@ -0,0 +1,13 @@
+package com.example.cqngu.doggie;
+
+/**
+ * Created by cqngu on 3/18/2016.
+ */
+public class Dog {
+    String name;
+    String type;
+    String note;
+    int id;
+
+    public Dog()
+}
